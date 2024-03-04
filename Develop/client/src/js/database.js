3ff913e1902a96c1f825917a873bb603b64467c5
elroy <devinjl665@gmail.com>
2024-03-03 19:36:39 -0600

    Added logic that accepts some content and adds to db and logic that retrieves all content from db

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,39 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
 
+export const putDb = async (content) => {
+  try{
+    console.log ('PUT to databse');
+    const textDB = await openDB('jate', 1);
+    const tx = textDB.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    await store.add(content);
+    await tx.done;
+
+    console.log('Content added to the database:', content);
+  } catch (error) {
+    console.error('Unable to add content to database:', error); 
+  }  
+};
+       
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+
+export const getDb = async () => {
+  try{
+    console.log('GET all from database');
+    const allTextDB = await openDB('jate', 1);
+    const allTx = allTextDB.transaction('jate', 'readonly');
+    const store = allTx.objectStore('jate');
+    const content = await store.getAll();
+    await allTx.done;
+
+    console.log('All content retrieved from database:', content);
+    return content;
+  } catch (error) {
+    console.log('Unable to retrieve content from database:', error);
+    return [];
+  }
+};
 
 initdb();
